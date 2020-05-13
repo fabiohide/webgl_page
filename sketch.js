b586910fb12c0d267c8e2f7472c5f9f1361e312a
fabiohide <59976172+fabiohide@users.noreply.github.com>
2020-05-13 13:09:47 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,50 @@
+sizeW = 42;
+sizeH = 36;
+W = 3840;
+H = 2160;
+
 function preload() {
 
-
+    cato = loadImage('cato.png');
     corpo1 = loadModel('pvc.obj');
 
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
+
+    background(0);
 }
 
 function draw() {
 
-    //angle = 0.07;
+    translate(0, 0, ((windowWidth + windowHeight) / 08));
+    translate(mouseY / 40, mouseX / 40);
+    // fill(255, 0, 0);
 
 
-    background(175);
+    // ellipse(10, 10, 10, 10);
 
-    //rectMode(CORNER);
-    //noStroke();
-    normalMaterial()
-    rotateY(frameCount * 0.15);
+
+    rotateY(frameCount * 0.05);
     rotateX(frameCount * 0.03);
-    model(corpo1, 100, 100);
+
+    for (y = -90; y <= width; y += sizeH) {
+        for (x = -176; x <= height; x += sizeW) {
+            fill(255, 0, 0);
+            //ellipse(x, y, sizeW, sizeH);
+            //image(cato, x, y, sizeW, sizeH);
+            normalMaterial()
+            model(corpo1);
+            translate(y, x);
+
+        }
+    }
+
+
+    //normalMaterial()
+    // rotateY(frameCount * 0.15);
+    //rotateX(frameCount * 0.03);
+    //model(corpo1, 100, 100);
 
 }
